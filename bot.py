d91b73507e5676e28780c3e96f9b26a9d0a5cc0a
Quiet Terminal Interactive <kmathers@kmathers.co.uk>
2024-11-17 02:11:07 +0000

    Added slash commands

--- bot.py
+++ bot.py
@@ -5,16 +5,16 @@ import discord
 from discord.ext import commands
 start_time = time.strftime('%m-%d-%YdT%H%M%S', time.localtime())
 
-#config
+# Config
 log_level = logging.INFO
 
-#filepaths
+# Filepaths
 token_file = './token.txt'
 logfilepath = './logs'
 logfile = 'Bot-' + start_time + '.log'
 
 
-#logging setup
+# Logging setup
 if not os.path.exists(logfilepath):
     os.makedirs(logfilepath)
 logging.basicConfig(filename= logfilepath + '/' + logfile, level=log_level)
@@ -39,11 +39,11 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 async def on_ready():
     logging.info(f'Logged in as {bot.user} and ready to go!')
 
-@bot.command(pass_context=True)
+@bot.slash_command(pass_context=True)
 async def ping(ctx):
-    logging.info(f'{ctx.message.author} ran command .ping in {ctx.guild.name}-{ctx.channel.name}')
-    await ctx.send('Pong')
+    logging.info(f'{ctx.user} ran command /ping in {ctx.guild.name}-{ctx.channel.name}')
+    await ctx.response.send_message('Pong')
 
 
-#login
+# Login
 bot.run(TOKEN)
